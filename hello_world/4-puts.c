733f953dde11abd46b97e94af95929f7abfda2f2
Emi-H106 <emi.hatano@.yahoo.com>
2025-02-14 09:10:38 +0000

    fix betty documentaionstyle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - the entry point of a C program
+ *
+ *
+ * Return: Always 0.
+ */
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
