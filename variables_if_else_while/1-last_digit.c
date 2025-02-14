7a36674327489e633b0c2a729f217f01070873de
Emi-H106 <emi.hatano@.yahoo.com>
2025-02-14 14:15:12 +0000

    fix script

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,9 +15,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 5)
 	{
 		printf("Last digit of %d and is greater than 5\n",n);
+	}
 	else if (n == 0)
 	{
 		printf("Last digit of %d and is 0\n",n);
