215987c2e525a32f77268c9fee031454df6ade16
Emi-H106 <emi.hatano@.yahoo.com>
2025-02-17 09:36:36 +0000

    write script

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	while (alphabet <= 'z')
+	{
+		alphabet++;
+
+		if (alphabet = 'e')
+		{
+			break;
+		}
+		else if (alphabet = 'q')
+		{
+			break;
+		}
+
+	}
+	
+
+	putchar('\n');
+	return (0);
+
+}	
+	
