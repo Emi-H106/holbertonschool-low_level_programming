7bfb829011ca5ac2ffd352a4d53a556171d8f401
Emi-H106 <emi.hatano@.yahoo.com>
2025-02-17 15:09:55 +0000

    fix script

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,27 +1,25 @@
-#include <stdio.h>
 #include "main.h"
 
 
 /**
- * main - check the code
+ * print_alphabet-prints the alphabet, in lowercase
  *
- * Return: Always 0.
+ * Return: void.
  */
 
 
-int main(void);
+
+void print_alphabet(void);
 {
-	void print_alphabet(void);
+	char c;
+
+	for (c = 'a'; c =< 'z'; c++)
 	{
-		char c;
-
-		for (c = 'a'; c =< 'z'; c++)
-		{
-			_putchar(c);
-		}
-
-		_putchar('\n');
-		return (0);
+		_putchar(c);
 	}
+
+	_putchar('\n');
+	return (0);
 }
+
 
