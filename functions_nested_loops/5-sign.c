258416368a580ed785e91e9b06c51d4abb50b859
Emi-H106 <emi.hatano@yahoo.com>
2025-02-18 13:01:41 +0000

    fix script

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar("+");
 		return (1);
 	}
 	else if (n = 0)
 	{
-		_putchar('0');
+		_putchar("0");
 		return (0);
 	}
 	else
 	{
-		_putchar('-');
+		_putchar("-");
 		return (-1);
 	}
 }
