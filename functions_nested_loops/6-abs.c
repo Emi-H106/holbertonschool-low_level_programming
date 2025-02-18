2a0b9d1351c6d35b084c1e6d5e6c9db5610756e1
Emi-H106 <emi.hatano@yahoo.com>
2025-02-18 13:50:52 +0000

    write script

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs: Display the absolute value of an integer
+ * 
+ * Return: n or -n
+ */
+
+int _abs(int)
+{
+	int n;
+	if (n < 0)
+	{
+		return -n;
+	}
+	else
+	{
+		return n;
+	}
+}
