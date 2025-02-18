a1a1bcfa9ebbe34b3a8fe9c25137abf0134435a2
Emi-H106 <emi.hatano@yahoo.com>
2025-02-18 11:57:36 +0000

    fix betty

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isalpha: Check if a character is an alphabet letter
- * @c: character for check ,type int
+ * _isalpha - Check if a character is an alphabet letter
+ * @c: The character for check
  * Return: 1 if c is a letter, lowercase or uppercase,0 otherwise
  */
 
@@ -11,14 +11,14 @@ int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		return(1);
+		return (1);
 	}
 	else if (c >= 'A' && c <= 'Z')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-	return(0);
+	return (0);
 	}
-}	
+}
