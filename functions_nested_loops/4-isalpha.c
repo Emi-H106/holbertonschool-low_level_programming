963388d3fea6b20173bc323c8408f37aa07a9071
Emi-H106 <emi.hatano@yahoo.com>
2025-02-18 11:49:58 +0000

    write script

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha: Check if a character is an alphabet letter
+ * @c: character for check ,type int
+ * Return: 1 if c is a letter, lowercase or uppercase,0 otherwise
+ */
+
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return(1);
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		return(1);
+	}
+	else
+	{
+	return(0);
+	}
+}	
