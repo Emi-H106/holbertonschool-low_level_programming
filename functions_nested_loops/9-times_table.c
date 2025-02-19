7824eca0d58822b4168fc5c39278d30a41219e43
Emi-H106 <emi.hatano@yahoo.com>
2025-02-19 10:56:30 +0000

    fix script

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,30 +8,23 @@
 
 void times_table(void)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i <= 9; i++)
+		for (j = 0; j <= 9; j++)
 	{
-		_putchar((i * 0) + '0');
-		_putchar((i * 1) + '0');
-		_putchar((i * 2) + '0');
-		_putchar((i * 3) + '0');
-		_putchar((i * 4) + '0');
-		_putchar((i * 5) + '0');
-		_putchar((i * 6) + '0');
-		_putchar((i * 7) + '0');
-		_putchar((i * 8) + '0');
-		_putchar((i * 9) + '0');
+		int n = i * j
+		_putchar( n + '0');
 
-			if ((i * 9) / 9 == 9)
-			{
-				_putchar('\n');
-			}
+		if (n / 9 == 9)
+		{
+			_putchar('\n);
+		}
+		else
+		{
+			_putchar(' ');
+			_putchar(',');
+		}
 
-			else
-			{
-				_putchar(' ');
-				_putchar(',');
-			}
 	}
 }
