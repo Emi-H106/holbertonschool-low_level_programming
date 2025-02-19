228c17daa5e3dcf42197d863cce5c8376db09158
Emi-H106 <emi.hatano@yahoo.com>
2025-02-19 09:45:37 +0000

    write script

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i * 9 + '0');
+		
+			if (i <= 8)
+			{
+				_putchar(' ');
+				_putchar(',');
+			}
+
+			else if (i = 9)
+			{
+				_putchar('\n');
+			}
+	}
+}
