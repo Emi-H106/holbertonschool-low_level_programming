8e2ccfacdd8fa5c513a5116cb9f44ccb986e28d8
Emi-H106 <emi.hatano@yahoo.com>
2025-02-21 12:28:18 +0000

    fix script

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,33 +9,27 @@
 
 
 void print_to_98(int n)
-{
-	 if (n == 98)
-          {
-		 _putchar((n / 10) + '0');
-		 _putchar((n % 10) + '0');
-	  }	
-		if (n <= 98)
+{	
+	if (n <= 98)
+	{
+		for (n = 0; n < 98; n++)
 		{
-			for (n = 0; n < 98; n++)
+			if (n < 10)
 			{
-				if (n < 10)
-				{
 				_putchar(n + '0');
 				_putchar(',');
 				_putchar(' ');
-				}
-				else
-				{
+			}
+			else
+			{
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
 				_putchar(',');
 				_putchar(' ');
-				}
 			}
-		
-			
 		}
+					
+	}
 	else if (n > 98)
 	{
 		for (n = 0; n > 98; n--)
@@ -55,8 +49,13 @@ void print_to_98(int n)
 				_putchar(',');
 				_putchar(' ');
 			}
-
 		}
+	}	
+	else
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
 	}
+
 	_putchar('\n');	
 }
