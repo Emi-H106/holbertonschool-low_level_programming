602a3535f842dfb5220de7193967ccaedf64c20d
Emi-H106 <emi.hatano@yahoo.com>
2025-02-21 14:49:44 +0000

    fix script

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,17 +16,17 @@ void times_table(void)
 		{
 			 k = i * j;
 			
-			if (k == 0)
+			if (k < 10)
 			{
 				_putchar(k + '0');
-			}
-			else if (10 < k)
-			{
+			
+				 if (j > 0)  
+				{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(k + '0');
-			}
+				}
+			}	 
 			else
 			{
 				_putchar(',');
