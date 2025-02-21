c0864ee511a59b3b7fe15c9c974ee539e22174ef
Emi-H106 <emi.hatano@yahoo.com>
2025-02-21 12:31:36 +0000

    fix script

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,9 +10,9 @@
 
 void print_to_98(int n)
 {	
-	if (n <= 98)
+	if (n < 98)
 	{
-		for (n = 0; n < 98; n++)
+		for (n = 0; n <= 98; n++)
 		{
 			if (n < 10)
 			{
@@ -32,7 +32,7 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (n = 0; n > 98; n--)
+		for (n = 0; n >= 98; n--)
 		{
 			if (n >= 100)
 			{
