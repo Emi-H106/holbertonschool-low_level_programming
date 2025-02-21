829598ebf2c9073386d99e00dedaea7e17a7090a
Emi-H106 <emi.hatano@yahoo.com>
2025-02-21 09:20:44 +0000

    write script

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: the number
+ * Return: void
+ */
+
+
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+		for (n = 0; n > 98; n--)
+
+		if (n < 10)
+		{
+			_putchar(n + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		else if (n == 98)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar('\n');
+		else
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+}
