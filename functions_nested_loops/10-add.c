2e683744737940f517e0e5f9272460c9fb8102b6
Emi-H106 <emi.hatano@yahoo.com>
2025-02-21 08:21:29 +0000

    write script

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int add - adds two integers
+ * @num1: number1,@num2: number2,@sum:total
+ * Return: the result
+ */
+
+
+int add(int, int)
+{
+	int num1, num2, sum;
+	sum = num1 + num2;
+	return (sum);
+}
