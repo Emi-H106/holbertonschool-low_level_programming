15afe0b68c698006fa241f9592ad7830f21d47c8
Emi-H106 <emi.hatano@yahoo.com>
2025-02-24 08:57:19 +0000

    write script

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: int
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+
+int _isupper(int c);
+{
+	for (c = 65; c <= 90; c++)
+		return (1);
+
+	for (c = 97; c <= 122; c++)
+		return (0);
+}
