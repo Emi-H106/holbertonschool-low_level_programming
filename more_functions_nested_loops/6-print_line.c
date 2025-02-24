215f89c991153593a691e45fd84460c8731ff00e
Emi-H106 <emi.hatano@yahoo.com>
2025-02-24 12:20:04 +0000

    fix script

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -8,13 +8,15 @@
 
 void print_line(int n)
 {
-	if (n == 0 && n < 0)
+	for (n = 0; n >= 1; n++)
 	{
-		_putchar('\n');
-	}
-	for (n = 1; n >= 1; n++)
-	{
-		_putchar('_');
+		if (n == 0 && n < 0)
+		{
+			_putchar('\n');
+		else
+		{
+			_putchar('_');
+		}
 	}
 	_putchar('\n');
 }
