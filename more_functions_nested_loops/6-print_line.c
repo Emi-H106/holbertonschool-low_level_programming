5b3eb85dfcda741f460e75c260c065670d79769e
Emi-H106 <emi.hatano@yahoo.com>
2025-02-24 11:51:21 +0000

    write script

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: the number of times the character
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	while (; ; n++)
+	{
+		if (n == 0 && n < 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
