a51abf266ea0556fb6449602dd5915af0fd26c43
Emi-H106 <emi.hatano@yahoo.com>
2025-02-25 14:22:48 +0000

    write script

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n:a pointer the integer we want to set to 98
+ * Return: void
+ */
+
+
+void reset_to_98(int *n)
+{
+	int a, n;
+
+	n = &a;
+	*n = 98;
+
+	return (*n);
+}
