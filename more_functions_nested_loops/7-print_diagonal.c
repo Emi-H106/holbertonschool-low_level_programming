49793c6214be06f1e114c06375e142d97a762208
Emi-H106 <emi.hatano@yahoo.com>
2025-02-25 09:04:12 +0000

    fix script

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,18 +9,23 @@
 
 void print_diagonal(int n)
 {
-	int a = 1;
-	int b;
+	int a, b;
 
 	if (n > 0)
 	{
-		while (a <= n)
+		for (a = 0; a < n; a++)
 		{
 			for (b = 0; b <= a ; b++)
 			{
-				_putchar(32);
+				if (b == a)
+				{
+					_putchar(92);
+				}
+				else
+				{
+					_putchar(32);
+				}
 			}
-			 _putchar(92);
 			 _putchar('\n');
 		}
 	}
