244adebe311aea45c31310faf445674f5b8779a8
Emi-H106 <emi.hatano@yahoo.com>
2025-02-25 09:15:51 +0000

    fix script

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -12,14 +12,14 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-	for (i = 0; i <= size; i++)
-	{
-		for (j = 0; j <= size; j++)
+		for (i = 0; i <= size; i++)
 		{
-			_putchar(35);
+			for (j = 0; j <= size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
 		}
-	}
-		_putchar('\n');
 	}
 	else
 	{
