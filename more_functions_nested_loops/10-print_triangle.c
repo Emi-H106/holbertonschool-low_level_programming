8afb59ee9d8339fbe573a8469e809b430233687f
Emi-H106 <emi.hatano@yahoo.com>
2025-02-25 12:10:41 +0000

    fix script

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,16 +10,23 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	for (i = 1; i <= size; i++)
+	if (size > 0)
 	{
-		for (j = 1; j <= size - i; j++)
+		for (i = 1; i <= size; i++)
 		{
-		_putchar(' ');
-		}
-		for (j = 1; j <= i; j++)
-		{
-			_putchar('#');
-		}
+			for (j = 1; j <= size - i; j++)
+			{	
+			_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}	
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
