fec662420d89473f528d6c8ad483332eb8b5fc71
Emi-H106 <emi.hatano@yahoo.com>
2025-02-26 12:56:48 +0000

    write script

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: a string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	char *end = s, temp;
+
+	while (*end)
+	{
+		end++;
+	}
+	end--;
+
+	while (s < end)
+	{
+		temp = *s;
+		*s = *end;
+		*end = temp;
+		s++;
+		end--;
+	}
+}
