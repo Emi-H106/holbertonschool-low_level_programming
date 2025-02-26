5afd24aa4eb4aa15913f569918f3b3df96a8ec88
Emi-H106 <emi.hatano@yahoo.com>
2025-02-26 09:55:49 +0000

    write script

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s:a string 
+ * Return: the length of a string
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (*s++)
+	{
+		c++;
+	}
+
+	return (c);
+}
