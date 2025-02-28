01465675d32093414e7187cb9c2ae52af16bc541
Emi-H106 <emi.hatano@yahoo.com>
2025-02-28 09:05:54 +0000

    fix script

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+
+/**
+ * puts_half - prints half of a string
+ * @str: a string
+ * Return: void
+ */
+
+
+void puts_half(char *str)
+{
+	int length_of_the_string = 0;
+	int mid, n;
+
+	while (str[length_of_the_string] != '\0')
+	{
+		length_of_the_string++;
+	}
+
+		if (length_of_the_string % 2 == 0)
+		{
+			mid = length_of_the_string / 2;
+			
+			for (n = mid; n < length_of_the_string; n++)
+			{
+				_putchar(str[n]);
+			}
+		}
+		else
+		{
+			mid = (length_of_the_string + 1) / 2;
+
+			for (n = mid; n < length_of_the_string; n++)
+			{
+				_putchar(str[n]);
+			}
+		}
+		_putchar('\n');
+}
