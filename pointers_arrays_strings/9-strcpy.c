22b7fc58b8df0887c3f332cb48a614c0c68b2280
Emi-H106 <emi.hatano@yahoo.com>
2025-02-28 12:44:31 +0000

    fix script

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -12,6 +12,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	while ((*dest++ = *src++) != '\0')
-		return (dest);
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
