8454e3c0ddfecb2c7ad15ea31dd5f0ed91494eaa
Emi-H106 <emi.hatano@yahoo.com>
2025-03-01 17:34:54 +0000

    fix script

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+/**
+ * *_strncat - concatenates two strings
+ * @dest: a pointer to the destination array,
+ * @src: the string to be appended
+ * @n: it will use at most n bytes
+ * Return: dest
+ */
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *temp = dest;
+	int i;
+
+	while (*temp != '\0')
+	{
+		temp++;
+	}
+
+	while (*src != '\0' && n > 0)
+	{
+		*temp = *src;
+		temp++;
+		src++;
+	}
+
+	*temp = '\0';
+	*temp = *dest;
+
+	return (dest);
+}
