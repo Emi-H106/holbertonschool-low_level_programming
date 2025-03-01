72c3449451c7928c52ef05a0802c14ccf8d81108
Emi-H106 <emi.hatano@yahoo.com>
2025-03-01 16:54:38 +0000

    write script

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: a pointer to the destination array
+ * @src: the string to be appended
+ * Return: dest
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+	char *temp;
+
+	temp = dest;
+
+	while (*temp)
+	{
+		temp++;
+	}
+
+	while (*src)
+	{
+		*temp = *src;
+		temp++;
+		src++;
+	}
+
+	*temp = '\0';
+}
