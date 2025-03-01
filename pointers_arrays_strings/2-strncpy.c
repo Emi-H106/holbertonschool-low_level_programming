0b2d8b94f63e504e2e0af9f6122157947c7c792b
Emi-H106 <emi.hatano@yahoo.com>
2025-03-01 23:47:41 +0000

    fix script

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,14 +12,17 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	n = 0;
+	int i;
 
-	while (src[n] != '\0')
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[n] = src[n];
-		n++;
+		dest[i] = src[i];
 	}
 
-	dest[n] = '\0';
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
 	return (dest);
 }
