6e36f9c12e77ccf4869627ffff220cd4e9c0a536
Emi-H106 <emi.hatano@yahoo.com>
2025-03-02 14:28:13 +0000

    write script

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * *leet - encodes a string into 1337.
+ * @str: a string
+ * Return: str
+ */
+
+
+ char *leet(char *str)
+{
+	char alphabet[10] = {'a', 'A' ,'e', 'E', 'o', 'O', 't' ,'T', 'l', 'L'};
+	char number[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	int i, j;
+	
+	for (i = 0; str[i] != '\0', i++)
+	{
+		for (j = 0, alphabet[j], j++)
+		{
+			if (str[i] == alphabet[j])
+			{
+				str[i] = number(j);
+				break;
+			}
+	}
+	return (str);
+}
