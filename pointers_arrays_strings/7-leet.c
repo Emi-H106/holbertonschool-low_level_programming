bb4741cdc137321708652ad3bb0f729cdbf46b10
Emi-H106 <emi.hatano@yahoo.com>
2025-03-02 14:41:00 +0000

    fix betty

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,12 +8,12 @@
  */
 
 
- char *leet(char *str)
+char *leet(char *str)
 {
-	char alphabet[10] = {'a', 'A' ,'e', 'E', 'o', 'O', 't' ,'T', 'l', 'L'};
+	char alphabet[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	char number[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 	int i, j;
-	
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
