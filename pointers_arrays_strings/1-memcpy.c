4abb64c7d7e17ca91b6d376dcbd5f2766a08ddd3
root <emi.hatano@yahoo.com>
2025-03-03 14:19:59 +0000

    fix script

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -13,12 +13,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	char *tmp = *src;
-
-	for (i = 0; i < n; i++)
+	
+	for (i = 0; i < n; n++)
 	{
-		dest = tmp[i];
-		
+		dest[i] = src[i];
 	}
+
 	return (dest);
 }
