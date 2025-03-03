c8cf41e55763891edd832b68a1b4d2e95c722acb
root <emi.hatano@yahoo.com>
2025-03-03 13:52:36 +0000

    comment

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -21,3 +21,17 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	return (s);
 }
+
+
+/**
+ * or,
+ * char *_memset(char *s, char b, unsigned int n) 
+ * {
+ *   unsigned int i;
+ *
+ *   for (i = 0; i < n; i++) 
+ *   {
+ *      s[i] = b;
+ *    }
+ *   return s;
+ */
