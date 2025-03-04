273585e687e755f7559205c030962ea564e8060d
root <emi.hatano@yahoo.com>
2025-03-04 14:18:16 +0000

    fix script

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -19,22 +19,21 @@ char *_strstr(char *haystack, char *needle)
 	{
 		return (haystack);
 	}
-	else
-	{
-		for (i = 0; haystack[i] != '\0'; i++)
-		{
-			if (*(haystack + i) == *needle)
-			{
 
-			for (j = k, k = 0; *(haystack + j) == *(needle + k); j++, k++)
-			{
-				if (*(haystack + K) == '\0')
-				{
-					return (haystack + i);
-				}
-			}
-			}
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h != '\0' && *n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
 		}
+
+	if (*n == '\0')
+	{
+		return (haystack);
 	}
 	return (NULL);
 }
