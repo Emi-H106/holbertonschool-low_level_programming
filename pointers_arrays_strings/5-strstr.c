9ad377ff808e6a4642471c764a188c4387873f8b
root <emi.hatano@yahoo.com>
2025-03-04 11:24:47 +0000

    fix )

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -16,11 +16,11 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle != '\0'; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i] == needle[j])
 			{
-				return (&haystack[i++];
+				return (&haystack[i++]);
 			}
 		}
 	}
