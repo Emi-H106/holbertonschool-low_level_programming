492487dad690b59fa078197e0c77bd18e583e00b
root <emi.hatano@yahoo.com>
2025-03-04 11:21:42 +0000

    write script

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * *_strstr - locates a substring.
+ * @haystack: the main string to be examined
+ * @needle: the sub-string to be searched in string
+ * Return: a pointer to the beginning of the located substring,
+ *         or NULL if the substring is not found.
+ */
+
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+			{
+				return (&haystack[i++];
+			}
+		}
+	}
+	return (NULL);
+}
