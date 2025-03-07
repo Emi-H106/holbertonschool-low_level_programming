4c0142d5d30b6ad9a301c010b259b8e1386a473b
root <emi.hatano@yahoo.com>
2025-03-07 18:03:36 +0000

    fix betty

---
+++ argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	printf("%d\n", argc);
+
+	for (i = 0; i < argc - 1; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
