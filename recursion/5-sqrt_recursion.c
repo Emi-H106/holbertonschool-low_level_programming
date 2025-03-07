6798bea757cddf479c66a81a523cb5f0197fab8c
root <emi.hatano@yahoo.com>
2025-03-07 12:42:26 +0000

    write script

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_sqrt_recursion - returns the natural square root of a number
+ * @n: the number
+ * Return: returns the natural square root of a number
+ */
+
+
+int _sqrt_recursion(int n)
+{
+	int i;
+
+	if ((i * i) > n)
+	{
+		return (-1);
+	}
+	else if ((i * i) == n)
+	{
+		return (n);
+	}
+	else
+	{
+		return (_sqrt_recursion(n, i + 1));
+	}
+}
