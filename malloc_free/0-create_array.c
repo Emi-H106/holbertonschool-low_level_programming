902de7916cbc789bcd3324ec3e9e5615f8dbb13b
root <emi.hatano@yahoo.com>
2025-03-10 13:13:26 +0000

    fix array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	char *array  = (char *)malloc(size * sizeof(char));
+	*array  = (char *)malloc(size * sizeof(char));
 
 	if (array == NULL)
 	{
