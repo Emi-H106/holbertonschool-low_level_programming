c7d675570aa4800d58a0d7c2da2129ed2374960f
root <emi.hatano@yahoo.com>
2025-03-11 07:59:02 +0000

    fix betty

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,46 +14,25 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	int i, j, k;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
+	if (s1 == NULL) s1 = "";
 
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
+	if (s2 == NULL) s2 = "";
 
 	i = 0;
 
-	while (s1[i] != '\0')
-	{
-		i++;
-	}
+	while (s1[i] != '\0') i++;
 
 	j = 0;
 
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
+	while (s2[j] != '\0') j++;
 
 	str = malloc(sizeof(char) * (i + j + 1));
 
-	if (str == NULL)
-	{
-		return (NULL);
-	}
+	if (str == NULL) return (NULL);
 
-	for (k = 0; k < i; k++)
-	{
-		str[k] = s1[k];
-	}
+	for (k = 0; k < i; k++) str[k] = s1[k];
 
-	for (k = 0; k < j; k++)
-	{
-		str[i + k] = s2[k];
-	}
+	for (k = 0; k < j; k++) str[i + k] = s2[k];
 
 	str[i + j] = '\0';
 	return (str);
