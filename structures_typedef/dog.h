bc86af7c4ad9f57a135a8d507e5bbb76f2050745
root <emi.hatano@yahoo.com>
2025-03-12 17:24:39 +0000

    structures

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * struct dog - Dog information
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
