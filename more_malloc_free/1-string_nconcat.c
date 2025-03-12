c1f2543a8735d3f9972cb74975214c031025c16f
root <emi.hatano@yahoo.com>
2025-03-12 12:22:24 +0000

    fix char

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -17,6 +17,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0;
 	unsigned int j = 0;
 	unsigned int k;
+	char *result;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -33,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > j)
 		n = j;
 
-	char *result = (char *)malloc(i + n + 1);
+	result = (char *)malloc(i + n + 1);
 
 	if (result == NULL)
 		return (NULL);
