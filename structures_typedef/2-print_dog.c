42ee15e6b7505194c5dd629187672966dc5350ae
root <emi.hatano@yahoo.com>
2025-03-13 12:09:15 +0000

    fix printf

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 
 	if (d == NULL)
 	{
-		return (NULL);
+		;
 	}
 
 	for (i = 0; i < 3; i++)
@@ -22,8 +22,8 @@ void print_dog(struct dog *d)
 		{
 			printf("if name is NULL, print Name: (nil)");
 		}
-		printf("Name: %s\n", d[0]);
-		printf("Age: %d\n", d[1]);
-		printf("Owner: %s\n", d[2]);
+		printf("Name: %s\n", d->name);
+		printf("Age: %d\n", d->age);
+		printf("Owner: %s\n", d->owner);
 	}
 }
