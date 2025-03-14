0f1679d6a2f588a19df60551350e59e929f498a7
root <emi.hatano@yahoo.com>
2025-03-14 10:48:40 +0000

    fix strcpy

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -79,7 +79,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	_strcpy(new_dog->name, name);
+	_strcpy(name, new_dog->name);
 
 	new_dog->age = age;
 
@@ -99,7 +99,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	_strcpy(new_dog->owner, owner);
+	_strcpy(owner, new_dog->owner);
 
 	return (new_dog);
 }
