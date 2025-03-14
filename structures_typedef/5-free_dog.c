d112c6e4de0c0b4e9c89f886cb3ff841648be0a0
root <emi.hatano@yahoo.com>
2025-03-14 14:23:27 +0000

    fix free

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -12,23 +12,17 @@
 
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-	{
-		;
-	}
-
-	if (d->name != NULL)
-	{
-		free(d->name);
-	}
-
-	if (d->owner != NULL)
-	{
-		free(d->owner);
-	}
-
 	if (d != NULL)
 	{
-		free(d);
+		if (d->name != NULL)
+		{
+			free(d->name);
+		}
+
+		if (d->owner != NULL)
+		{
+			free(d->owner);
+		}	
+	free(d);
 	}
 }
