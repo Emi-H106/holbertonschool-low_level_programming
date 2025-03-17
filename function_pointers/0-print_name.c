b8d8c7bb1faed15e74d2611035388073f4d025ec
root <emi.hatano@yahoo.com>
2025-03-17 13:57:18 +0000

    write function

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,2 +1,13 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - prints a name
+ * @name: a pointer of name
+ * @f: a pointer of function
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
