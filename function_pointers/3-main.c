4ed8ef36e3bf324f8c002cddf08e77298c64912c
root <emi.hatano@yahoo.com>
2025-03-19 09:14:30 +0000

    fix case 99

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 
 	operator = get_op_func(argv[2]);
 
-	if (operator != '+' && operator != '-' && operator != '*' && operator != '/' && operator != '%')
+	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' && *argv[2] != '/' && *argv[2] != '%')
 	{
 		printf("Error\n");
 		exit(99);
