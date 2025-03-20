76464c03d0517adb3fe7614b7eeb403e6c3f3fa0
root <emi.hatano@yahoo.com>
2025-03-20 14:49:55 +0000

    in progress

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -17,31 +17,44 @@ void print_all(const char * const format, ...)
 	va_start(args, format);
 
 	const char *ptr = format;
+	int printed = 1;
 
 	while (*ptr)
 	{
-		if (format == 'c')
+		while (*ptr == 'c' || *ptr == 'i' || *ptr == 'f' || *ptr == 's')
 		{
-			printf("%c", va_arg(args, char);
-		}
-		else if (format == 'i')
-		{
-			printf("%d", va_arg(args, int);
-		}
-		else if (format == 'f')
-		{
-			printf("%f", va_arg(args, double);
-		}
-		else if (format == 's')
-		{
-			*str = format;
+			if (!printed)
+			{
+				printf(",");
+			}
+			printed = 0;
 
-			if (str == NULL)
+		if (*ptr == 'c')
+		{
+			printf("%c", va_arg(args, char));
+		}
+		else if (*ptr == 'i')
+		{
+			printf("%d", va_arg(args, int));
+		}
+		else if (*ptr == 'f')
+		{
+			printf("%f", va_arg(args, float));
+		}
+		else if (*ptr == 's')
+		{
+			char str = va_arg(args, char*);
+			if (*str == NULL)
 			{
 				printf("(nill)");
 			}
-			printf("%s", va_arg(args, char);
+			else
+			{
+			printf("%s", va_arg(args, char));
+			}
+		ptr++;
+		printed = 1;
 		}
-	var_end(args);
+	va_end(args);
 	printf("\n");
 }
