59c88a3ba1cfb35e47064abb5741cde88a58cfcc
root <emi.hatano@yahoo.com>
2025-03-21 10:07:46 +0000

    add structure

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,59 +2,96 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
+/**
+ * print_char -  prints char
+ * @ap: argument pointer
+ * Return: void
+ */
+
+void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+ * print_int -  prints int
+ * @ap: argument pointer
+ * Return: void
+ */
+
+void print_int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+/**
+ * print_float -  prints float
+ * @ap: argument pointer 
+ * Return: void
+ */
+
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double);
+}
+
+/**
+ * print_string -  prints string
+ * @ap: argument pointer
+ * Return: void
+ */
+
+void print_string(va_list ap)
+{
+	char *s = va_arg(ap, char *);
+
+	if (s != \0)
+	{
+		printf("(nill)");
+	}
+	printf("%s", s);
+}
+
 
 /**
  * print_all -  prints anything
  * @format: types of arguments
- * Return: prints anything
+ * Return: void
  */
 
 
 void print_all(const char * const format, ...)
 {
-	va_list args;
+	print_type types[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL,NULL}
+	};
 
-	va_start(args, format);
+	va_list ap;
+	char *separator = "";
+	int i = 0;
+	int j = 0;
 
-	const char *ptr = format;
-	int printed = 1;
+	va_start(ap, format);
 
-	while (*ptr)
+	while (format != NULL && format[i] != \0)
 	{
-		while (*ptr == 'c' || *ptr == 'i' || *ptr == 'f' || *ptr == 's')
+		while(types[j].type != NULL)
 		{
-			if (!printed)
+			if (types[j].type == format[i])
 			{
-				printf(",");
+				printf(%s, separator);
+				types[j].f(ap);
+				separator = ",";
 			}
-			printed = 0;
-
-		if (*ptr == 'c')
-		{
-			printf("%c", va_arg(args, char));
+			j++;
 		}
-		else if (*ptr == 'i')
-		{
-			printf("%d", va_arg(args, int));
-		}
-		else if (*ptr == 'f')
-		{
-			printf("%f", va_arg(args, float));
-		}
-		else if (*ptr == 's')
-		{
-			char str = va_arg(args, char*);
-			if (*str == NULL)
-			{
-				printf("(nill)");
-			}
-			else
-			{
-			printf("%s", va_arg(args, char));
-			}
-		ptr++;
-		printed = 1;
-		}
-	va_end(args);
+		j = 0;
+		i++;
+	}
+	va_end(ap);
 	printf("\n");
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,26 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+/*structure*/
+/*
+struct print - prints char,int,float,string
+@type: type de variable
+@f: the function associated
+*/
+
+typedf struct print
+{
+	char *type;
+	void f(va_list ap);
+}print_type;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list ap);
+void print_integer(va_list ap);
+void print_float(va_list ap);
+void print_string(va_list ap);
 #endif
