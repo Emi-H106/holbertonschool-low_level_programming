d58150c1810af47cdf801e435cbae95a827b41e7
root <emi.hatano@yahoo.com>
2025-04-01 06:58:51 +0000

    fix betty style

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,20 +4,20 @@
 
 
 /**
- * add_node - adds a new node at the beginning of a list_t list
- * @head: a pointer to a pointer  the first node of the list
- * @str: The string to be stored in the new node 
- * Return: the address of the new node, or NULL if it fail
+ * _strlen - calculates the length of the string
+ * @s: the string
+ * Return: the length of a string
  */
 
 size_t _strlen(const char *s)
 {
 	size_t length = 0;
+
 	while (s[length] != '\0')
 	{
 		length++;
 	}
-    return length;
+	return (length);
 }
 
 
@@ -25,7 +25,7 @@ size_t _strlen(const char *s)
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: a pointer to a pointer  the first node of the list
- * @str: The string to be stored in the new node 
+ * @str: The string to be stored in the new node
  * Return: the address of the new node, or NULL if it failed
  */
 
@@ -51,12 +51,12 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (new_node->str == NULL)
 	{
-		free (new_node);
+		free(new_node);
 		return (NULL);
 	}
 
 	new_node->len = _strlen(str);
 	new_node->next = *head;
 	*head = new_node;
-	return(new_node);
+	return (new_node);
 }
