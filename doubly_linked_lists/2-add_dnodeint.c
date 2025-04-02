cad4ecd04aa3241cad16f67563776d5420496d1c
root <emi.hatano@yahoo.com>
2025-04-02 07:33:41 +0000

    fix error malloc

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,15 +14,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
 	new_node->n = n;
 	new_node->next = *head;
 	new_node->prev = NULL;
-
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
 
 	if ((*head) != NULL)
 	{
