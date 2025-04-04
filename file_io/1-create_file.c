59a0dee3f22944e899823c0ad25e60cd4510b0ee
Emi-H106 <emi.hatano@yahoo.com>
2025-04-04 15:57:52 +0200

    fix return 0 case:filename=NULL

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -34,7 +34,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t bytes_written;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
@@ -53,5 +53,5 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	close(fd);
-	return (-1);
+	return (1);
 }
