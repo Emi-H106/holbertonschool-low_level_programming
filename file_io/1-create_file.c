ccfc75cce256602182fbdec9123d98fdee05d71b
Emi-H106 <emi.hatano@yahoo.com>
2025-04-04 15:51:31 +0200

    create function

---
+++ file_io/1-create_file.c
@@ -0,0 +1,57 @@
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include"main.h"
+
+/**
+ * _strlen - calculates the length of the string
+ * @s: the string
+ * Return: the length of a string
+ */
+
+size_t _strlen(const char *s)
+{
+	size_t length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+
+/**
+ * create_file - creates a file
+ * @filename: a pointer to filename
+ * @text_content: a pointer to text content
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, _strlen(text_content));
+
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (-1);
+}
