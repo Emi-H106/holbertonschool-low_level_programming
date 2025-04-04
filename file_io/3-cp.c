f1dc53d0ae428241a0f4df151e4f3b2d4f93409a
Emi-H106 <emi.hatano@yahoo.com>
2025-04-04 19:14:51 +0200

    fix in progress

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 	if (fd_from == -1)
 		print_error("Error: Can't read from file %s\n", argv[1], 98);
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
 	if (fd_to == -1)
 		print_error("Error: Can't write to %s\n", argv[2], 99);
